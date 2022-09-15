5860fdc0ca7ed1d513399c7c4553f627704277a5
richmans <richmans@users.noreply.github.com>
2022-09-15 19:35:23 +0200

    moving routes to /admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,20 @@ Rails.application.routes.draw do
   get '/published/:podcast/podcast', to: 'hosting#fetch_podcast'
   get '/published/:podcast/:filename', to: 'hosting#fetch_recording'
   get '/published/*path', to: 'hosting#not_found'
-  resources :recordings
-  resources :podcasts do
-    resources :privileges
+  scope '/admin' do
+    resources :recordings
+    resources :podcasts do
+      resources :privileges
+    end
+    resources :users
+    get '/render/:podcast_name', to: 'render#show'
+    post '/switch_podcast', to: 'sessions#switch_podcast'
+    get '/noprivilege', to: 'request_podcast#index'  
   end
-  get '/render/:podcast_name', to: 'render#show'
-  post '/switch_podcast', to: 'sessions#switch_podcast'
-  get '/noprivilege', to: 'request_podcast#index'
-  resources :users
-  root "home#index"
+  
   get '/login', to: 'sessions#login'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
   post '/logout', to: 'sessions#destroy'
-  
+  root "home#index"
 end
